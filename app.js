aa835f73058c06c47a77676c955ca22d42046c3e
Miguel Diaz <miguelzaiid@gmail.com>
2023-06-27 21:41:35 -0500

    Cargue de proyecto

---
+++ app.js
@@ -0,0 +1,7 @@
+let cuentas = [
+    {nombre: 'Miguel', saldo: 200, password: 'User123'},
+    {nombre: 'Felipe', saldo: 200, password: 'User234'},
+    {nombre: 'Ximena', saldo: 200, password: 'User567'}
+];
+
+console.log(cuentas);
