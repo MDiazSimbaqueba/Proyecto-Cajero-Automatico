ec1f5396003aa5604c63bbee14fcaae96c2c2fff
Miguel Diaz <miguelzaiid@gmail.com>
2023-06-29 19:32:23 -0500

    Avance JS

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let accounts = [
     {nombre: 'Miguel', saldo: 200, password: 'User123'},
-    {nombre: 'Felipe', saldo: 200, password: 'User234'},
-    {nombre: 'Ximena', saldo: 200, password: 'User567'}
+    {nombre: 'Felipe', saldo: 200, password: 'User456'},
+    {nombre: 'Ximena', saldo: 200, password: 'User789'}
 ];
-// console.log(cuentas[0].nombre);
-// let userId = 'Miguel';
-// let userPassword = 'User123';
+// console.log(cuentas[].nombre);
 //Encapsula la información de los input
 const btnAccess = document.getElementById("btn-access");
 const userId = document.getElementById("user-id");
 const userPassword = document.getElementById("user-password");
+let textInfo = document.getElementById("text-info");
 
+//Validación de credenciales al dar clic en el botón
 btnAccess.addEventListener("click", function(event) {
     event.preventDefault();
     const credential = {
@@ -19,30 +19,15 @@ btnAccess.addEventListener("click", function(event) {
     }
     console.log(credential);
     for (let i = 0; i < accounts.length; i++) {
-        if (credential == accounts[i]) {
-            console.log("Todo correcto");
-            break;         
-        } else{
-            console.log("Usuario y/o contraseña inválidos");
-        }
+        console.log(accounts[i].nombre);
+        if (credential.userId == accounts[i].nombre) {
+            if (credential.userPassword == accounts[i].password) {
+                console.log("Todo correcto");
+                break;   
+            } else {
+                textInfo.textContent = "Usuario y/o contraseña inválidos";
+                textInfo.style.color = "#ff1979";
+            }
+        } 
     }
 });
-// let userId = 'Miguel';
-// let userPassword = 'User123';
-// // let userId = 'Felip';
-// // let userPassword = 'User123';
-
-
-
-// //Comparamos si los datos ingresados son correctos
-// for (let i = 0; i < accounts.length; i++) {
-//     if (userId == accounts[i].nombre) {
-//         console.log("Todo correcto");
-//         break;         
-//     } else{
-//         console.log("Usuario y/o contraseña inválidos");
-//     }
-// }
-
-// while (condition) {
-    
